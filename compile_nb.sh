88446ff7589851cf58723f125ab45bb2f61e2940
Jay Reiter <jayr2@illinois.edu>
2021-07-21 22:44:37 -0500

    added PD code pipeline

--- compile_nb.sh
+++ compile_nb.sh
@@ -1,5 +1,11 @@
+# compile get_eigs notebook
 jupyter nbconvert --to python get_eigs.ipynb
-
 echo "if __name__ == \"__main__\":
     import sys
     get_knot_eigs(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))" >> get_eigs.py
+
+# compile parse_pd_code notebook
+jupyter nbconvert --to python parse_pd_code.ipynb
+echo "if __name__ == \"__main__\":
+    import sys
+    parse_pd_code(sys.argv[1])" >> parse_pd_code.py
--- run_all.sh
+++ run_all.sh
@@ -11,6 +11,7 @@ do
     while [ $j -le ${arr[$i-3]} ]
     do
         # create dir for knot differentials if it doesn't exist
+        # TODO: fix dir creation for general pd code iteration
         dir="./KhoHo/differentials/knot_${i}_${j}"
         mkdir $dir
 
@@ -20,6 +21,8 @@ do
         j=$(($j + 1))
     done
 done
+
+python3 parse_pd_code.py "./pd_code_input.txt"
 
 # compute all knot differentials
 cd KhoHo
@@ -37,8 +40,6 @@ echo "$(cat ../gp_out)" | gp -f -q -s 120000000 KH unpack_matrix.gp
 # parallel --jobs 6 gp -f -q -s 120000000 KH unpack_matrix.gp :::: gp_out
 
 echo "finished parallel part"
-
-
 cd ..
 
 # compute all eigenvalues
@@ -50,6 +51,10 @@ do
         dir="./KhoHo/differentials/knot_${crossings}_${index}"
         python3 get_eigs.py ${dir} ${crossings} ${index}
         echo ${crossings} ${index}
+
+        # remove all the differentials
+        rm -r "./KhoHo/differentials/knot_${crossings}_${index}"
+
         index=$(($index + 1))
     done
 done
