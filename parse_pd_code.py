7ac32b2e87e7a36dcb85e57bc585491ff228d19d
Jay Reiter <jayr2@illinois.edu>
2021-07-26 20:56:13 -0500

    modified time output

--- parse_pd_code.py
+++ parse_pd_code.py
@@ -4,16 +4,17 @@
 # In[ ]:
 
 
-def parse_pd_code(path, start_index):
+def parse_pd_code(path):
     with open(path, "r") as file:
         lines = file.readlines()
         crossing_lines = lines[3::5]
         pd_codes = lines[6::5]
         for index in range(len(pd_codes)):
-            pd_code = pd_codes[index][1:].replace("),", ");").strip()
+            pd_code = pd_codes[index][1:].replace("),", ";").replace("(","").replace(")","")
             crossings = crossing_lines[index].split("random_link(")[1].split(",")[0]
-            print("compute_pd_code_differential(" + pd_code + ", " + str(crossings) + ", " + str(index+start_index) + ")")
+            print("compute_pd_code_differential(" + pd_code[:-1] + ", " + str(crossings) + ", " + str(index) + ")")
+# parse_pd_code("./pd_code_input.txt")
 
 if __name__ == "__main__":
     import sys
-    parse_pd_code(sys.argv[1], int(sys.argv[2]))
+    parse_pd_code(sys.argv[1])
