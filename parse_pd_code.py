5e20b5c1e7d5d43d7f37f86f7c9553ba8f59eeea
Jay Reiter <jayr2@illinois.edu>
2021-09-04 17:44:33 -0500

    switched parsing method

--- parse_pd_code.py
+++ parse_pd_code.py
@@ -4,26 +4,28 @@
 # In[ ]:
 
 
-def parse_pd_code(path):
-    with open(path, "r") as file:
-        lines = file.readlines()
-        crossing_lines = lines[3::5]
-        pd_codes = lines[6::5]
-        for index in range(len(pd_codes)):
-            pd_code = pd_codes[index][1:].replace("),", ";").replace("(","").replace(")","")
-            crossings = crossing_lines[index].split("random_link(")[1].split(",")[0]
-            print("compute_pd_code_differential(" + pd_code[:-1] + ", " + str(crossings) + ", " + str(index) + ")")
-# parse_pd_code("./pd_code_input.txt")
-
 # def parse_pd_code(path):
 #     with open(path, "r") as file:
 #         lines = file.readlines()
-#         for line in lines:
-#             parts = line.split(";")
-#             pd_code = parts[2].replace("),", ";").replace("(","").replace(")","")
-#             crossings = int(parts[0])
-#             index = (crossings - 4) * 50 + int(parts[1])
+#         crossing_lines = lines[3::5]
+#         pd_codes = lines[6::5]
+#         for index in range(len(pd_codes)):
+#             pd_code = pd_codes[index][1:].replace("),", ";").replace("(","").replace(")","")
+#             crossings = crossing_lines[index].split("random_link(")[1].split(",")[0]
 #             print("compute_pd_code_differential(" + pd_code[:-1] + ", " + str(crossings) + ", " + str(index) + ")")
+# parse_pd_code("./pd_code_input.txt")
+
+def parse_pd_code(path):
+    with open(path, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            parts = line.split(";")
+            pd_code = parts[2].replace("),", ";").replace("(","").replace(")","")
+            crossings = int(parts[0])
+            index = (crossings - 4) * 50 + int(parts[1])
+            print("compute_pd_code_differential(" + pd_code[:-1] + ", " + str(crossings) + ", " + str(index) + ")")
+# parse_pd_code("./pd_code_input.txt")
+
 
 
 if __name__ == "__main__":
